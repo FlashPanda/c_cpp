639735285390a7bfbddaeefa11611bda9e2c7cbc
virtual_company <caizhoudong001@gmail.com>
2018-07-02 09:03:21 +0800

    chdir的使用方法

--- chdir.cpp
+++ chdir.cpp
@@ -1,9 +1,13 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main () {
-	printf ("当前工作目录：%s\n", getcwd (NULL, NULL));
+	char curDir[256] = {0};
+	getcwd (curDir, 256);
+	printf ("当前工作目录：%s\n", curDir);
 	
 	chdir ("/tmp");
 	
-	printf ("切换之后的工作目录：%s\n", getcwd (NULL, NULL));
+	getcwd (curDir, 256);
+	printf ("切换之后的工作目录：%s\n", curDir);
 }
