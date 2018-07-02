3ec7bf899f1f2e7755e9971e595ead24053e1e09
virtual_company <caizhoudong001@gmail.com>
2018-07-02 09:20:09 +0800

    chdir的修改

--- chdir.cpp
+++ chdir.cpp
@@ -6,8 +6,15 @@ int main () {
 	getcwd (curDir, 256);
 	printf ("当前工作目录：%s\n", curDir);
 	
-	chdir ("/tmp");
+	int result = chdir ("/tmp");
+	printf ("切换的结果是：%d\n", result);
 	
 	getcwd (curDir, 256);
 	printf ("切换之后的工作目录：%s\n", curDir);
+
+	int result1 = chdir ("/tmp/sse/sie/sse/112/");
+	printf ("切换的结果是%d\n", result1);
+
+	getcwd (curDir, 256);
+	printf ("切换后的目录是：%s\n", curDir);
 }
