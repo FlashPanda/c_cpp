634f5d20effc04cdc3c98ec40fd8607ea90d3aca
Panda <caizhoudong001@gmail.com>
2022-04-05 20:34:35 +0800

    eight queen unfinished

---
+++ algorithms/eight_queen.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+// 没完成呢.
+struct QueryStruct
+{
+    int result[8] = {-1};
+    int checkRow = -1;
+    int checkColumn = -1;
+};
+
+std::vector<std::vector<int>> result;
+bool PlaceQueen(int row)
+{
+    for (int i = 0; i < 8; ++i)
+    {
+        if (IsValid(row, i))
+
+    }
+}
+
+void EightQueen()
+{
+    int quireRow = 0;
+    int quireColumn = 0;
+
+    while (quireRow < 8 || quireColumn < 8)
+    {
+
+    }
+    int i = 0;
+    while (i < 8)
+    {
+
+    }
+    for (int i = 0; i < 8; ++i)
+    {
+        std::vector<int> result;
+    }
+}
+
+void main()
+{
+
+}
